5b36d16f5b9059eb0d25e12419f3b21e856853da
iltds <iltds@proton.me>
2023-11-09 11:57:06 -0300

    add rofi suport

--- config.def.h
+++ config.def.h
@@ -72,49 +72,51 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "kitty", NULL };
-static const char *browser[] =  { "vivaldi", NULL };
+static const char *dmenucmd[] 	= { "dmenu_run",	NULL };
+static const char *rofi[] 	= { "rofi",   		"-show", "drun"};
+static const char *termcmd[]	= { "kitty", 		NULL };
+static const char *browser[]	= { "vivaldi",		NULL };
 
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_v, spawn,          {.v = browser } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },//esconder barra
-	{ MODKEY,                       XK_s,      focusstack,     {.i = +1 } },//trocar janela
-	{ MODKEY,                       XK_w,      focusstack,     {.i = -1 } },//trocar janela
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_u,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_a,      setmfact,       {.f = -0.05} },//controlar tamanho
-	{ MODKEY,                       XK_d,      setmfact,       {.f = +0.05} },//controlar tamanho
-	{ MODKEY,                       XK_space, zoom,           {0} },//alternar janela
-	{ MODKEY,                       XK_Tab,    view,           {0} },//alternar tag
-	{ MODKEY,	                XK_q,      killclient,     {0} },//fechar app
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_o,  	   setlayout,      {0} },//layout
-	{ MODKEY|ShiftMask,             XK_o,      togglefloating, {0} },//layout
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	/* modifier			key		function        argument */
+	{ MODKEY,			XK_backslash,	spawn,          {.v = rofi } },
+	{ MODKEY,                       XK_p,      	spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return, 	spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_v, 		spawn,          {.v = browser } },
+	{ MODKEY,                       XK_b,      	togglebar,      {0} },//esconder barra
+	{ MODKEY,                       XK_s,      	focusstack,     {.i = +1 } },//trocar janela
+	{ MODKEY,                       XK_w,      	focusstack,     {.i = -1 } },//trocar janela
+	{ MODKEY,                       XK_i,      	incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_u,      	incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_a,      	setmfact,       {.f = -0.05} },//controlar tamanho
+	{ MODKEY,                       XK_d,      	setmfact,       {.f = +0.05} },//controlar tamanho
+	{ MODKEY,                       XK_space, 	zoom,           {0} },//alternar janela
+	{ MODKEY,                       XK_Tab,    	view,           {0} },//alternar tag
+	{ MODKEY,	                XK_q,      	killclient,     {0} },//fechar app
+	{ MODKEY,                       XK_t,      	setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,      	setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      	setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_o,  	   	setlayout,      {0} },//layout
+	{ MODKEY|ShiftMask,             XK_o,      	togglefloating, {0} },//layout
+	{ MODKEY,                       XK_0,      	view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,      	tag,            {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,  	focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period, 	focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  	tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, 	tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_minus,  	setgaps,        {.i = -1 } },
+	{ MODKEY,                       XK_equal,  	setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  	setgaps,        {.i = 0  } },
+	TAGKEYS(                        XK_1,                      	0)
+	TAGKEYS(                        XK_2,                      	1)
+	TAGKEYS(                        XK_3,                      	2)
+	TAGKEYS(                        XK_4,                      	3)
+	TAGKEYS(                        XK_5,                      	4)
+	TAGKEYS(                        XK_6,                      	5)
+	TAGKEYS(                        XK_7,                      	6)
+	TAGKEYS(                        XK_8,                      	7)
+	TAGKEYS(                        XK_9,                      	8)
+	{ MODKEY|ShiftMask,             XK_q,      quit,           	{0} },
 };
 
 /* button definitions */
--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -9,9 +9,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#3f3f3f70", "#0f0f0f70" },
+	[SchemeSel] = { "#dfdfdf70", "#0f0f0f70" },
+	[SchemeOut] = { "#00ffff", "#ffff00" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
